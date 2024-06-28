b72c386299cb592cdba5c3f7fbf442af9524e03c
Darco-Darco <d.lokhai@unasat.sr>
2024-06-28 12:54:25 -0300

    Update DatabaseConnection.java

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/beroepsproduct";
     private static final String USER = "root";
-    private static final String PASSWORD = "StarWars_1010";
+    private static final String PASSWORD = "****";
     private static Connection connection;
 
     public static Connection getConnection() throws SQLException{
